200c5b83101b65fae4edb801198ad93c01e2d867
tanushbhootra576 <193266446+tanushbhootra576@users.noreply.github.com>
2025-07-22 17:56:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LandignNew from './componets/LandingCompo/LandingNew';
 import About from './componets/About/About';
 import Gallery from './componets/Gallery/Gallery';
 import NavBar from './componets/NavBar/NavBar';
-import SidePanel from './componets/SidePanel/SidePanel'; 
+import SidePanel from './componets/SidePanel/SidePanel';
 
 const App = () => {
   const [menuOpen, setMenuOpen] = useState(false);
- 
 
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
   };
 
   return (
-    <Router>
+    <>
       <NavBar toggle={toggleMenu} />
       <SidePanel isOpen={menuOpen} toggle={toggleMenu} />
       <Routes>
@@ -23,7 +22,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/gallery" element={<Gallery />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
